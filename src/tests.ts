3cae478ce38f15b556bb4d8ee9d73a5b67623ea3
Jan Romaniak <romaniakjan@gmail.com>
2021-01-03 13:42:44 +0100

    Changed to whole base

--- src/tests.ts
+++ src/tests.ts
@@ -3,47 +3,6 @@ export const tests = [
   {
     id: "0",
     title: "Gineksy",
-    questions: [
-      {
-        title: "Pytanie nr 1",
-        answers: [
-          "ból w miednicy lub jamie brzusznej.",
-          "oliguria.",
-          "powiększenie obwodu brzucha.",
-          "wzdęcia, uczucie pełności.",
-          "parcia naglące."
-        ],
-        question: "Do objawów raka jajnika nie należy:",
-        correctAnswear: 1,
-        id: "0"
-      },
-      {
-        title: "Pytanie nr 2",
-        answers: [
-          "razem z pępowiną i błonami płodowymi tworzy popłód.",
-          "masa łożyska w przybliżeniu stanowi 500g.",
-          "o wykształconym łożysku można mówić dopiero w połowie II trymestru.",
-          "zraz łożyska to jednostka morfologiczno-czynnościowa łożyska.",
-          "żyła pępkowa prawa zanika we wczesnym okresie życia płodowego."
-        ],
-        question: "Wskaż fałszywe stwierdzenie dotyczące łożyska:",
-        correctAnswear: 2,
-        id: "1"
-      },
-      {
-        title: "Pytanie nr 3",
-        answers: [
-          "endometriozę miednicy mniejszej.",
-          "wrodzone wady macicy.",
-          "niewydolność szyjki macicy.",
-          "mięśniaki macicy.",
-          "wszystkie powyższe."
-        ],
-        question:
-          "Do czynników anatomicznych, które mogą być przyczyną poronień, należy zaliczyć:",
-        correctAnswear: 4,
-        id: "2"
-      }
-    ]
+    questions: questionsGineksy
   }
 ];
